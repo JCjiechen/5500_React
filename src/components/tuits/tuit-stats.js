bb2b2f73e6468e4c6d1bec5a684c8c40522ee402
JC <jc.jiechen.jane@gmail.com>
2022-03-29 00:59:11 -0400

    update thumbs

--- src/components/tuits/tuit-stats.js
+++ src/components/tuits/tuit-stats.js
@@ -7,36 +7,40 @@ const TuitStats = ({ tuit, likeTuit, dislikeTuit = () => { } }) => {
         <i className="far fa-message me-1"></i>
         {tuit.stats && tuit.stats.replies}
       </div>
+
       <div className="col">
         <i className="far fa-retweet me-1"></i>
         {tuit.stats && tuit.stats.retuits}
       </div>
+
       <div className="col">
         <span onClick={() => likeTuit(tuit)}>
           {
-            tuit.stats && tuit.stats.likes && tuit.stats.likes > 0 &&
-            <i className="fas fa-thumbs-up me-1" style={{ color: 'red' }}></i>
+            tuit.stats && tuit.stats.likes > 0 &&
+            <i className="fa-solid fa-thumbs-up me-1" style={{ color: 'red' }}></i>
           }
           {
-            tuit.stats && tuit.stats.likes && tuit.stats.likes <= 0 &&
-            <i className="far fa-heart me-1"></i>
+            tuit.stats && tuit.stats.likes <= 0 &&
+            <i className="fa-regular fa-thumbs-up me-1"></i>
           }
           {tuit.stats && tuit.stats.likes}
         </span>
       </div>
+
       <div className="col">
         <span onClick={() => dislikeTuit(tuit)}>
           {
-            tuit.stats && tuit.stats.dislikes && tuit.stats.dislikes > 0 &&
-            <i className="fas fa-thumbs-down me-1" style={{ color: 'red' }}></i>
+            tuit.stats && tuit.stats.dislikes > 0 &&
+            <i className="fa-solid fa-thumbs-down me-1" style={{ color: 'red' }}></i>
           }
           {
-            tuit.stats && tuit.stats.dislikes && tuit.stats.dislikes <= 0 &&
-            <i className="far fa-heart me-1"></i>
+            tuit.stats && tuit.stats.dislikes <= 0 &&
+            <i className="fa-regular fa-thumbs-down me-1"></i>
           }
           {tuit.stats && tuit.stats.dislikes}
         </span>
       </div>
+
       <div className="col">
         <i className="far fa-inbox-out"></i>
       </div>
